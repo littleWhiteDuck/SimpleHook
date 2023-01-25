53068914a8cdf7aad3aa1a849b769a8180448a37
littleWhiteDuck <57512745+littleWhiteDuck@users.noreply.github.com>
2023-01-25 21:29:51 +0800

    优化读取记录逻辑

--- app/src/main/java/me/simpleHook/bean/RecordBean.kt
+++
@@ -1,9 +0,0 @@
-package me.simpleHook.bean
-
-import androidx.room.ColumnInfo
-
-
-data class RecordBean(
-    @ColumnInfo(name = "packageName") var packageName: String,
-    @ColumnInfo(name = "type") var type: String = "need_update",
-)
--- app/src/main/java/me/simpleHook/bean/RecordSummary.kt
+++
@@ -1,8 +0,0 @@
-package me.simpleHook.bean
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class RecordSummary(val type: String = "", val packageName: String = "", val count: Int = 0) :
-    Parcelable
