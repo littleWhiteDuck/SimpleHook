e82a021689db8074e394c130669a9b0b15a777bd
littleWhiteDuck <57512745+littleWhiteDuck@users.noreply.github.com>
2023-03-09 13:05:51 +0800

    修复配置页数组越界

--- app/src/main/java/me/simpleHook/constant/Constant.kt
+++ app/src/main/java/me/simpleHook/constant/Constant.kt
@@ -1,13 +1,6 @@
 package me.simpleHook.constant
 
 object Constant {
-    // app 整体hook模式
-    const val HOOK_ORIGIN = 0
-    const val HOOK_JIA_GU = 1
-
-    /* const val HOOK_360 = 1
-     const val HOOK_TENCENT = 2
-     const val HOOK_OTHER = 3*/
     // 方法Hook模式
     const val HOOK_RETURN = 0
     const val HOOK_PARAM = 1
