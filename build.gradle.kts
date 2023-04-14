a64045466ef0cd64fcb8e1fb812ab1b66a7b2911
littleWhiteDuck <57512745+littleWhiteDuck@users.noreply.github.com>
2023-04-14 19:16:40 +0800

    gap -> 8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.0.0")
         val kotlinVersion = "1.8.20-Beta"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
